585115e0743dc154e09b078d2c32750dc5532988
gagan1146 <gagansharma99690@gmail.com>
2025-10-18 21:45:43 +0530

    Use of useCallback

--- hooks-react/src/App.jsx
+++ hooks-react/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import UseState from './component/UseState';
 import UseEffect from './component/UseEffect';
+import UseCallback from './component/UseCallback';
 
 const App = () => {
   return (
     <>
-    <UseState/>
-    <UseEffect/>
+    {/* <UseState/> */}
+    {/* <UseEffect/> */}
+      <UseCallback/>
     </>
   );
 };
